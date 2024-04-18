48e27b8586297bc42266c1a867b83f17c1a40933
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-04-18 19:36:28 +0530

    Responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/BRAND.svg";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="Navbar">
-      <img src={logo} className="logo"></img>
-      <ul>
-        <li>About</li>
-        <li>Offers</li>
-        <li>Support</li>
-      </ul>
+      <img src={logo} className="logo" alt="Logo"></img>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        {menuOpen ? (
+          <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
+        ) : (
+          <i className="fa-solid fa-close" onClick={toggleMenu}></i>
+        )}
+      </div>
+      <div className="menu main">
+        <ul>
+          <li>About</li>
+          <li>Offers</li>
+          <li>Support</li>
+        </ul>
+      </div>
+      <div className={`menu ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>About</li>
+          <li>Offers</li>
+          <li>Support</li>
+        </ul>
+        <div className="auth2">
+          Sign in
+          <i className="fa-brands fa-google"></i>
+        </div>
+      </div>
       <div className="auth">
         Sign in
-        <i class="fa-brands fa-google"></i>
+        <i className="fa-brands fa-google"></i>
       </div>
     </div>
   );
